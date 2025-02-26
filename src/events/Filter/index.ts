5b86bf1113064b7ce823c7698a4c7456f5c9c3ca
Vinicius Diniz <vinicius.diniz@somagrupo.com.br>
2025-02-26 17:03:54 -0300

    Incluindo doc sobre o evento de filtro

--- src/events/Filter/index.ts
+++ src/events/Filter/index.ts
@@ -1,6 +1,17 @@
 import { sendEventToDataLayer } from "../Common";
 import { FilterProps } from "./types";
 
+/**
+ * @param filterProps - Objeto com as propriedades do filtro
+ * @returns void
+ * @description Envia um evento de filtro para o dataLayer
+ * @example
+ * sendFilterEventToDataLayer({
+ *    color: 'red'
+ * })
+ * // Irá disparar um evento de filtro com os parâmetros { event: 'filter', color: 'red' }
+ */
+
 export function sendFilterEventToDataLayer(filterProps: FilterProps) {
     sendEventToDataLayer<FilterProps>({
         event: 'filter',
