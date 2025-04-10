f877152a6682e7bf60ba368cb128938271ce8cd7
André Menezes <andre.menezes@farmrio.com>
2025-04-10 18:04:00 -0300

    update usetracking

--- src/hooks/useTracking/index.ts
+++ src/hooks/useTracking/index.ts
@@ -2,8 +2,9 @@ import { clearTracking } from './clearTracking';
 import { getTrackingByProductId } from './getTrackingById';
 import { removeTracking } from './removeTracking';
 import { setTracking } from './setTracking';
+import { UseTrackingProps } from './types';
 
-export function useTracking() {
+export function useTracking(): UseTrackingProps {
   return {
     get: getTrackingByProductId,
     set: setTracking,
